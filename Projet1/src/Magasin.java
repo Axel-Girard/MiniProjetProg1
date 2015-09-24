0518741c33f3000fc796f71fd01a17e7a91729ba
Axel Girard <axel.girard@etu.univ-nantes.fr>
2015-09-24 10:03:45 +0200

    changement main

--- Projet1/src/Magasin.java
+++ Projet1/src/Magasin.java
@@ -1,8 +1,20 @@
 import java.util.ArrayList;
 
 public class Magasin {
-	ArrayList<Article> article = new ArrayList<Article>();
+	private ArrayList<Article> article;
 
+	public Magasin(){
+		article = new ArrayList<Article>();
+	}
+
+	public ArrayList<Article> getArticle() {
+		return article;
+	}
+	public void setArticle(ArrayList<Article> article) {
+		this.article = article;
+	}
+
+	// Classe main
 	public static void main(String[] args) {
 		System.out.println("Welcome in the magasin of the future of demain !");
 	}
