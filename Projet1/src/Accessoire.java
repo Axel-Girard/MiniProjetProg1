47d45585d2c8b9f19855a68eb308a3aa2712c694
Axel Girard <axel.girard@etu.univ-nantes.fr>
2015-09-24 09:54:07 +0200

    test 4

--- Projet1/src/Accessoire.java
+++ Projet1/src/Accessoire.java
@@ -1,13 +1,4 @@
 /* commentaire x2 HD Remix of the heart */
 public abstract class Accessoire {
-	public void test(){
-		int bob = 3;
-	}
-	
 
-	public void test(){
-		int a = 2;
-		int b = 3;
-		int c = a+b;
-	}
 }
