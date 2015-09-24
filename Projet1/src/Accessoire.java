e7d22d61224387dec8f5fe29fa062fe035fbf80c
Axel Girard <axel.girard@etu.univ-nantes.fr>
2015-09-24 10:08:35 +0200

    Accessoire

--- Projet1/src/Accessoire.java
+++ Projet1/src/Accessoire.java
@@ -1,4 +1,18 @@
+import java.util.HashSet;
+
 /* commentaire x2 HD Remix of the heart */
 public abstract class Accessoire {
+	private HashSet<Marque> marque;
 
+	public Accessoire(){
+		marque = new HashSet<Marque>();
+	}
+
+	public HashSet<Marque> getMarque() {
+		return marque;
+	}
+
+	public void setMarque(HashSet<Marque> marque) {
+		this.marque = marque;
+	}
 }
