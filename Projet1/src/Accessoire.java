505b5dad42e63bea7a573a979ec18b5e5b584bb8
ludovic.chevraux <ludovic.chevraux@etu.univ-nantes.fr>
2015-09-24 10:00:57 +0200

    Ajout classe Accessoire

--- Projet1/src/Accessoire.java
+++ Projet1/src/Accessoire.java
@@ -1,4 +1,21 @@
-/* commentaire x2 HD Remix of the heart */
+import java.util.HashSet;
+import java.util.Set;
+
+
 public abstract class Accessoire {
+	private Set marque;
+
+	public Set getMarque() {
+		return marque;
+	}
+	public void setMarque(Set marque) {
+		this.marque = marque;
+	}
+	public Accessoire() {
+		this.marque = new HashSet();
+	}
+	
+	
+	
 
 }
